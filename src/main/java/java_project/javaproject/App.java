42a758bb94feed8c2a6a4cf27f374ec2971a3647
Yashwanth1140 <akulayashwanth43@gmail.com>
2025-08-18 10:10:18 +0530

    Inital maven project commit

---
+++ src/main/java/java_project/javaproject/App.java
@@ -0,0 +1,13 @@
+package java_project.javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
